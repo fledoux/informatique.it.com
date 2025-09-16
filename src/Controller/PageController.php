a7413f7bfb4da64a1dded1a98c9ccfe0ea0556d9
Frédéric Ledoux <fledoux@users.noreply.github.com>
2025-09-16 17:54:02 +0200

    dash

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use App\Form\ContactType;
+use App\Repository\TicketRepository;
+use App\Repository\CompanyRepository;
+use App\Repository\ContactRepository;
 
 final class PageController extends AbstractController
 {
@@ -20,6 +23,65 @@ final class PageController extends AbstractController
         }
         $form = $this->createForm(ContactType::class);
         return $this->render('page/home.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[IsGranted('PUBLIC_ACCESS')]
+    #[Route('/dashboard', name: 'app_dashboard', methods: ['GET'])]
+    public function index(
+        TicketRepository $ticketRepo,
+        CompanyRepository $companyRepo,
+        ContactRepository $contactRepo
+    ): Response {
+        $now = new \DateTimeImmutable();
+
+        // Compteurs
+        $ticketsCount    = $ticketRepo->count([]);
+        $companiesCount  = $companyRepo->count([]);
+        $contactsCount   = $contactRepo->count([]);
+
+        // Statuts utiles (doivent exister dans ta data)
+        $openStatuses    = ['new', 'in_progress', 'waiting'];
+        $closedStatuses  = ['resolved', 'closed', 'canceled'];
+
+        // Tickets ouverts
+        $openTicketsCount = $ticketRepo->createQueryBuilder('t')
+            ->select('COUNT(t.id)')
+            ->andWhere('t.ticket_status IN (:st)')
+            ->setParameter('st', $openStatuses)
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        // En attente
+        $waitingCount = $ticketRepo->createQueryBuilder('t')
+            ->select('COUNT(t.id)')
+            ->andWhere('t.ticket_status = :st')
+            ->setParameter('st', 'waiting')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        // En retard (due_at passée et pas clôturé)
+        $overdueCount = $ticketRepo->createQueryBuilder('t')
+            ->select('COUNT(t.id)')
+            ->andWhere('t.due_at IS NOT NULL')
+            ->andWhere('t.due_at < :now')
+            ->andWhere('t.ticket_status NOT IN (:closed)')
+            ->setParameter('now', $now)
+            ->setParameter('closed', $closedStatuses)
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        // Derniers tickets
+        $recentTickets = $ticketRepo->findBy([], ['id' => 'DESC'], 20);
+
+        return $this->render('page/dashboard.html.twig', [
+            'ticketsCount'     => (int)$ticketsCount,
+            'companiesCount'   => (int)$companiesCount,
+            'contactsCount'    => (int)$contactsCount,
+            'openTicketsCount' => (int)$openTicketsCount,
+            'waitingCount'     => (int)$waitingCount,
+            'overdueCount'     => (int)$overdueCount,
+            'recentTickets'    => $recentTickets,
+        ]);
     }
 
     #[IsGranted('PUBLIC_ACCESS')]
