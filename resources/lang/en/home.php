167d45f26c187c889313294e93a25132e4f84060
Frédéric Ledoux <fledoux@users.noreply.github.com>
2025-09-24 08:19:27 +0200

    home

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'Welcome' => 'Professional IT Support - informatique.it.com',
-    'RGPD' => 'Privacy Policy (GDPR) - informatique.it.com',
-    'Legal' => 'Legal Notice - informatique.it.com',
-    'CGV' => 'Terms and Conditions - informatique.it.com',
+    'Welcome' => 'Professional IT Support',
+    'RGPD' => 'Privacy Policy (GDPR)',
+    'Legal' => 'Legal Notice',
+    'CGV' => 'Terms and Conditions',
 ];
