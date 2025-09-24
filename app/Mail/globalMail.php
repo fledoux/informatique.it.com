ce273e5ff86884ea7f733dbb0436a100ce589e5d
Frédéric Ledoux <fledoux@users.noreply.github.com>
2025-09-24 11:06:10 +0200

    mail

--- app/Mail/globalMail.php
+++ app/Mail/globalMail.php
@@ -9,7 +9,9 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class globalMail extends Mailable implements ShouldQueue
+// class globalMail extends Mailable implements ShouldQueue
+
+class globalMail extends Mailable
 {
     use Queueable, SerializesModels;
 
