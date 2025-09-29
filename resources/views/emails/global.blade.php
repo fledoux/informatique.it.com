e0c6efbcca2bcabadb848495eb0b92ca8035a868
Frédéric Ledoux <fledoux@users.noreply.github.com>
2025-09-29 17:30:44 +0200

    margin

--- resources/views/emails/global.blade.php
+++ resources/views/emails/global.blade.php
@@ -10,7 +10,7 @@
 <div
     style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border: 1px solid #dee2e6; border-radius: 0.375rem; box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075); padding: 1.5rem; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;">
     <img src="{{ $message->embed(public_path('assets/img/logo/logo-horizontal.svg')) }}" alt="informatique.it.com"
-        style="width:240px; margin-bottom: 40px !important;">
+        style="width:240px; margin-bottom: 60px !important;">
     <h1 style="color: #212529; font-size: 2rem; font-weight: bold; margin-bottom: 1rem; margin-top: 0;">
         {{ $title ?? 'Hello' }}
     </h1>
