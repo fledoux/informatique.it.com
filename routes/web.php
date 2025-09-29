8f1dde4849c1ed0ef76ea6c00e069ba2a0e97c99
Frédéric Ledoux <fledoux@users.noreply.github.com>
2025-09-29 17:23:57 +0200

    belair

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/legal', [PageController::class, 'legal'])->name('legal');
 Route::get('/rgpd', [PageController::class, 'rgpd'])->name('rgpd');
 Route::get('/cgv', [PageController::class, 'cgv'])->name('cgv');
 Route::get('/qr', [PageController::class, 'qr'])->name('qr');
+Route::get('/belair', [PageController::class, 'belair'])->name('belair');
 Route::get('/scannez-moi', [PageController::class, 'qrCode'])->name('qr-code');
 
 // Auth routes
