89a770df1f0a2b3af41d075565ffaa2315224312
Frédéric Ledoux <fledoux@users.noreply.github.com>
2025-10-03 10:19:06 +0200

    web

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -89,7 +89,7 @@ class PageController extends Controller
 
     public function qrCode()
     {
-        $dataUri = Page::generateQrCode('https://informatique-it.com/car');
+        $dataUri = Page::generateQrCode('https://informatique-it.com/web');
         return view('pages.qr-code', compact('dataUri'));
     }
 
