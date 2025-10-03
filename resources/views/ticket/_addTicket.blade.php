734fd6d91a1399d42ca80516fa0edcf9795a5340
Frédéric Ledoux <fledoux@users.noreply.github.com>
2025-10-03 18:31:27 +0200

    add

--- resources/views/ticket/_addTicket.blade.php
+++ resources/views/ticket/_addTicket.blade.php
@@ -4,25 +4,34 @@
     </button>
     <ul class="dropdown-menu">
         <li><a class="dropdown-item" href="{{ route('ticket.edit', $ticket) }}">
-            {!! __('ticket.Answer') !!}
-        </a></li>
+                {!! __('ticket.Answer') !!}
+            </a></li>
         @can('ticket.edit')
-        <li><a class="dropdown-item" href="{{ route('ticket.edit', $ticket) }}">
-            {!! __('global.Edit') !!}
-        </a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li>
-            <form action="{{ route('ticket.resend-confirmation', $ticket) }}" method="POST" class="d-inline">
-                @csrf
-                <button type="submit" class="dropdown-item" onclick="return confirm('Renvoyer l\'email de confirmation ?')">
-                    <i class="fa-regular fa-envelope me-2"></i>Renvoyer confirmation
-                </button>
-            </form>
-        </li>
-        <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="{{ route('ticket.edit', $ticket) }}">
+                    {!! __('global.Edit') !!}
+                </a></li>
+            @role('super-admin')
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li>
+                    <form action="{{ route('ticket.resend-confirmation', $ticket) }}" method="POST" class="d-inline">
+                        @csrf
+                        <button type="submit" class="dropdown-item"
+                            onclick="return confirm('Renvoyer l\'email de confirmation ?')">
+                            <i class="fa-regular fa-envelope me-2"></i>Renvoyer confirmation
+                        </button>
+                    </form>
+                </li>
+            @endrole
+            <li>
+                <hr class="dropdown-divider">
+            </li>
         @endcan
-        <li><a class="dropdown-item" href="{{ route('ticket.index') }}">
-            {!! __('global.Back') !!}
-        </a></li>
+        <li>
+            <a class="dropdown-item" href="{{ route('ticket.index') }}">
+                {!! __('global.Back') !!}
+            </a>
+        </li>
     </ul>
 </div>
