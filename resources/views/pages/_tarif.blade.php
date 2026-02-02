a407f236efe84c2d06a4379238af92c31dfd033c
Frédéric Ledoux <fledoux@users.noreply.github.com>
2026-02-02 18:48:05 +0100

    janvier

--- resources/views/pages/_tarif.blade.php
+++ resources/views/pages/_tarif.blade.php
@@ -27,7 +27,7 @@
                     </div>
                     <p class="text-secondary mb-0">
                         Tarifs au 1<sup>er</sup>
-                        septembre
+                        janvier
                         {{ date('Y') }}<br>1 ticket équivant à une intervention de 30 minutes.
                     </p>
                 </div>
