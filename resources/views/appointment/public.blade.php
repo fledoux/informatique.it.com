91e370f1fb6db5dc0791d92997fcfaf0cc1c5a20
Frédéric Ledoux <fledoux@users.noreply.github.com>
2026-06-17 11:09:59 +0200

    orange

--- resources/views/appointment/public.blade.php
+++ resources/views/appointment/public.blade.php
@@ -138,7 +138,7 @@
                                                 } elseif ($slotForTime['is_available']) {
                                                     $buttonClasses .= ' btn-outline-success ' . ($checked ? 'btn-success' : '');
                                                 } else {
-                                                    $buttonClasses .= ' btn-light text-body-orange border-orange border';
+                                                    $buttonClasses .= ' btn-outline-orange';
                                                 }
                                             @endphp
                                             <button
