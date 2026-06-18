43fc70188f602adfd1293ac96173f95ae4680c4b
Frédéric Ledoux <fledoux@users.noreply.github.com>
2026-06-18 08:56:40 +0200

    show

--- resources/views/appointment-project/show.blade.php
+++ resources/views/appointment-project/show.blade.php
@@ -144,19 +144,11 @@
                                         @php
                                             $slotDateTime = \Carbon\Carbon::parse($slot->slot_date . ' ' . $slot->slot_time)->format('Y-m-d H:i');
                                             $isBooked = $bookedSlots->contains($slotDateTime);
-                                            
-                                            if ($slot->break_text) {
-                                                $buttonClass = 'btn-primary';
-                                            } elseif ($isBooked) {
-                                                $buttonClass = 'btn-outline-orange';
-                                            } else {
-                                                $buttonClass = 'btn-outline-secondary';
-                                            }
                                         @endphp
                                         <form method="POST" action="{{ route('appointment-project.slots.destroy', [$project->id, $slot->id]) }}">
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" class="btn btn-sm {{ $buttonClass }}" title="Cliquer pour supprimer">
+                                            <button type="submit" class="btn btn-sm @if($slot->break_text) btn-primary @elseif($isBooked) btn-outline-orange @else btn-outline-secondary @endif" title="Cliquer pour supprimer">
                                                 {{ \Carbon\Carbon::parse($slot->slot_time)->format('H:i') }}
                                                 @if(!$slot->break_text && !$isBooked)
                                                     <i class="fa-solid fa-xmark ms-1 text-danger"></i>
